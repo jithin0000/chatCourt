666b010b3028548c92dcc2a0dabfced6f9d3376f
jithin0000 <mbjithinbabu@gmail.com>
2020-04-27 09:23:16 +0530

    profile updation completed

--- lawyer/forms.py
+++ lawyer/forms.py
@@ -1,2 +1,9 @@
 from django import forms
+from .models import Lawyer
 
+
+class LawyerUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Lawyer
+        fields = ['profile_pic','description','phone_number']
